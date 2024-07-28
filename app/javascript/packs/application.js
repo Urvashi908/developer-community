7be581c6b4be1c7f48f02f3668d91b705f2744e1
Urvashi <urvashinigam121@gmail.com>
2024-07-28 23:30:25 +0530

    stimules js and login_logut with device.

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+// app/javascript/packs/application.js
+import Rails from "@rails/ujs";
+Rails.start();
+
+// If you are using Turbo, add this as well:
+import { Turbo } from "@hotwired/turbo-rails";
