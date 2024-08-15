d409c2c4ef8757666a49e9b3f39cddb7eb546c43
Urvashi <urvashinigam121@gmail.com>
2024-08-15 09:47:31 +0530

    [WIP] try to get fix edit_discrption.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,11 @@ class MembersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit_description
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.turbo_stream # Renders edit_description.turbo_stream.erb
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,9 @@ Rails.start();
 
 // If you are using Turbo, add this as well:
 import { Turbo } from "@hotwired/turbo-rails";
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
